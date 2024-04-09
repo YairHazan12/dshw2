48ada3d038ce6c6d97d29cbda4f9f0b6a566bf13
Erel Edree <ereledreeuk@gmail.com>
2024-04-09 13:48:04 +0300

    heap

--- GeneralPurposeHeap.java
+++ GeneralPurposeHeap.java
@@ -1,3 +1,48 @@
-public class GeneralPurposeHeap {
+public class GeneralPurposeHeap<T extends Comparable<T>> {
+    private int size;
+    private T[] heap;
 
+    public GeneralPurposeHeap() {
+        T[] heap = new T[16];
+        this.heap = heap;
+        this.size = 0;
+    }
+
+    public GeneralPurposeHeap(int initialCapacity) {
+        T[] heap = new T[initialCapacity];
+        this.heap = heap;
+        this.size = 0;
+    }
+
+    public GeneralPurposeHeap(T[] initialData) {
+
+    }
+
+    public void insert(T element) {
+        if (this.heap.length = this.size)// if the heap is full
+        {
+
+        } else // if the heap is not full
+        {
+            PercUp(i, x, this.heap);
+        }
+    }
+
+    public T findMin() {
+
+    }
+
+    public int getSize() {
+        return -1;
+    }
+
+    public T deleteMin() {
+
+    }
+
+    public void mergeHeap(GeneralPurposeHeap<T> otherHeap) {
+
+    }
+
+    // helper functions
 }
